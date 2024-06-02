039bad1443c2bda328c13d9e9fe68090f219a615
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-06-02 11:16:11 -0700

    Variable Exercise

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declare and initialize variables
+string name = "Hugo";
+int age = 27;
+char middleInitial = 'D';
+bool isHungry = true;
+double  priceOfBurger= 8.99;
+decimal priceOfBurgerWithFries = 10.99m;
+
+// Writing variables to console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Hungry:{isHungry}");
+Console.WriteLine($"Price of Burger: {priceOfBurger}");
+Console.WriteLine($"Price of burger with fries: {priceOfBurgerWithFries}");
